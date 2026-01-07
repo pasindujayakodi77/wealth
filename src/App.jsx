33f46c996bb29055945adcfa5deb1853fe8618dc
pasindujayakoi77 <pasindujaya687@gmail.com>
2026-01-07 13:17:12 +0530

    feat: initialize project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the main application layout.
    - Developed App.jsx component with a responsive layout using Tailwind CSS.
    - Added React logo SVG in the assets directory.
    - Implemented productCard component with styling for product display.
    - Created superProductCard component to showcase featured products.
    - Configured main entry point in main.jsx for rendering the application.
    - Set up Vite configuration for React and Tailwind CSS integration.✏️

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="w-full h-screen bg-blue-300">
+            <div className="w-150 h-150 bg-black flex flex-col justify-center ">
+                <div className="w-75 h-75 bg-yellow-600"></div>
+                <div className="w-75 h-75 bg-red-900"></div>
+                <div className="w-75 h-75 bg-green-600"></div>
+                <div className="w-75 h-75 bg-purple-600"></div>
+                <div className="w-75 h-75 bg-pink-600"></div>
+                <div className="w-75 h-75 bg-orange-600"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
